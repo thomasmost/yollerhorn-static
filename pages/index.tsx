a917940744f06c028bd52cb2753108afd644a619
Thomas Constantine Moore <talk@thomas.tech>
2022-05-17 21:40:27 -0400

    adjust styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import type { NextPage } from "next";
 import Head from "next/head";
+// import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 import Logo from "./yollerhorn.svg";
@@ -27,15 +28,11 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>Yollerhorn</h1>
         <p className={styles.description}>
           Yollerhorn was a website started by Darren Caulley and Thomas
-          Constantine Moore in 2014.
+          Constantine Moore in 2014. It was pretty cool.
         </p>
         <p className={styles.description}>
-          We envisioned a sort of IBDB for downtown theater, an event network
-          for plays put on for one night only in someone&apos;s backyard in Bushwick,
-          for new works and new performers. It was pretty cool.
-        </p>
-        <p className={styles.description}>
-          We&apos;re working on other projects now, and they&apos;re pretty cool too.
+          We&apos;re working on other projects now, and they&apos;re pretty cool
+          too.
         </p>
         <div className={styles.grid}>
           <a href="https://bend.green" className={styles.card}>
@@ -58,6 +55,9 @@ const Home: NextPage = () => {
           >
             Thomas
           </a>
+          {/* <Link href="/about">
+            About
+          </Link> */}
 
           <a
             href="https://www.linkedin.com/in/darrencaulley/"
